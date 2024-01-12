19962d537d4827522ed6f586d69863707ba8632e
Adnan Bin Farook <adnanabf@gmail.com>
2024-01-12 19:34:49 +0530

    Added DbContext and Connection Strings

---
+++ VacaBook.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacaBook.Domain.Entities;
+
+namespace VacaBook.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>().HasData(new Villa
+            {
+                Id = 1,
+                Name = "Premium Pool Villa",
+                Description = "This is the description for the premium pool villa",
+                ImageUrl = "https://placehold.co/600x401",
+                Occupancy = 4,
+                Price = 1000,
+                Sqft = 1000
+            });
+        }
+    }
+}
---
+++ VacaBook.Infrastructure/Migrations/20240112140159_SeedVillaToDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacaBook.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[] { 1, null, "This is the description for the premium pool villa", "https://placehold.co/600x401", "Premium Pool Villa", 4, 1000.0, 1000, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
