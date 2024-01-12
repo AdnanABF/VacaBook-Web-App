86453ad3e478d725b85f751ab31344b059c0e65e
Adnan Bin Farook <adnanabf@gmail.com>
2024-01-12 20:02:27 +0530

    Added Villa Controller and View

---
+++ VacaBookWeb/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VacaBook.Infrastructure.Data;
+
+namespace VacaBook.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
