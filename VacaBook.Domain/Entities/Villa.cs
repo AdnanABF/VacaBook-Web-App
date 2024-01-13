236f751362796dbc9b0463154fc5c0d1b2c9f3e2
Adnan Bin Farook <adnanabf@gmail.com>
2024-01-13 08:27:08 +0530

    Added villa CRUD operations and toastr notifications

--- VacaBook.Domain/Entities/Villa.cs
+++ VacaBook.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,16 @@ namespace VacaBook.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(10,10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1,10)]
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
--- VacaBook.Web/Controllers/VillaController.cs
+++ VacaBook.Web/Controllers/VillaController.cs
@@ -1,25 +1,93 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VacaBook.Domain.Entities;
 using VacaBook.Infrastructure.Data;
 
 namespace VacaBook.Web.Controllers
 {
     public class VillaController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _DbContext;
 
         public VillaController(ApplicationDbContext context)
         {
-            _context = context;
+            _DbContext = context;
         }
         public IActionResult Index()
         {
-            var villas = _context.Villas.ToList();
+            var villas = _DbContext.Villas.ToList();
             return View(villas);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("description", "The description cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _DbContext.Villas.Add(villa);
+                _DbContext.SaveChanges();
+                TempData["success"] = "The villa has been created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            var villa = _DbContext.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+            if (ModelState.IsValid)
+            {
+                _DbContext.Villas.Update(villa);
+                _DbContext.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            var villa = _DbContext.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            var villaDetails = _DbContext.Villas.FirstOrDefault(x => x.Id == villa.Id);
+            if (villaDetails is not null)
+            {
+                _DbContext.Villas.Remove(villaDetails);
+                _DbContext.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa could not be deleted";
+            return RedirectToAction("Error", "Home");
         }
     }
 }
