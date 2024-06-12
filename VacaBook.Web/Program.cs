8470e0a308486e32da86b1bd1c725b4e59a3e3f9
Adnan Bin Farook <adnanabf@gmail.com>
2024-06-12 11:30:38 +0530

    Added Syncfusion Package to the project

--- VacaBook.Web/Program.cs
+++ VacaBook.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Stripe;
+using Syncfusion.Licensing;
 using System.Globalization;
 using VacaBook.Application.Common.Interfaces;
 using VacaBook.Domain.Entities;
@@ -39,6 +40,8 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
+SyncfusionLicenseProvider.RegisterLicense(builder.Configuration.GetSection("Syncfusion:Licensekey").Get<string>());
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
