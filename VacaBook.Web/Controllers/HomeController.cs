2f8e163f4997cce5b79d3d251502b187d53a5b94
Adnan Bin Farook <adnanabf@gmail.com>
2024-06-12 13:37:39 +0530

    Added Export option for exporting PPT

--- VacaBook.Web/Controllers/HomeController.cs
+++ VacaBook.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Hosting;
+using Syncfusion.Presentation;
 using System.Diagnostics;
+using System.Drawing;
 using VacaBook.Application.Common.Interfaces;
 using VacaBook.Application.Common.Utility;
 using VacaBook.Web.Models;
@@ -10,10 +13,12 @@ namespace VacaBook.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(IUnitOfWork unitOfWork)
+        public HomeController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -50,6 +55,104 @@ namespace VacaBook.Web.Controllers
             };
 
             return PartialView("_VillaList", homeViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePPTExport(int id)
+        {
+            var villa = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").FirstOrDefault(x => x.Id == id);
+            if (villa is null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            string basePath = _webHostEnvironment.WebRootPath;
+            string filePath = basePath + @"/exports/ExportVillaDetails.pptx";
+
+            using IPresentation presentation = Presentation.Open(filePath);
+
+            ISlide slide = presentation.Slides[0];
+
+            IShape? shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtVillaName") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Name;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtVillaDescription") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Description;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtVillaName") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Name;
+            }
+
+
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtOccupancy") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Max Occupancy: {0} adults", villa.Occupancy);
+            }
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtVillaSize") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Villa Size: {0} sqft", villa.Sqft);
+            }
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtPricePerNight") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("{0}/Night", villa.Price.ToString("c"));
+            }
+
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "txtVillaAmenitiesHeading") as IShape;
+            if (shape is not null)
+            {
+                List<string> listItems = villa.VillaAmenity.Select(x => x.Name).ToList();
+                shape.TextBody.Text = "";
+
+                foreach (var item in listItems)
+                {
+                    IParagraph paragraph = shape.TextBody.AddParagraph();
+                    ITextPart textPart = paragraph.AddTextPart(item);
+
+                    paragraph.ListFormat.Type = ListType.Bulleted;
+                    paragraph.ListFormat.BulletCharacter = '\u2022';
+                    textPart.Font.FontName = "system-ui";
+                    textPart.Font.FontSize = 18;
+                    textPart.Font.Color = ColorObject.FromArgb(144, 148, 152);
+                }
+            }
+
+            shape = slide.Shapes.FirstOrDefault(x => x.ShapeName == "imgVilla") as IShape;
+            if (shape is not null)
+            {
+                byte[] imageData;
+                string imageUrl;
+                try
+                {
+                    imageUrl = string.Format("{0}{1}", basePath, villa.ImageUrl);
+                    imageData = System.IO.File.ReadAllBytes(imageUrl);
+                }
+                catch (Exception)
+                {
+                    imageUrl = string.Format("{0}{1}", basePath, "/img/placeholder.png");
+                    imageData = System.IO.File.ReadAllBytes(imageUrl);
+                }
+
+                slide.Shapes.Remove(shape);
+                using MemoryStream imageStream = new(imageData);
+                IPicture newPicture = slide.Pictures.AddPicture(imageStream, 60, 120, 300, 200);
+            }
+
+            MemoryStream memoryStream = new();
+            presentation.Save(memoryStream);
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "application/pptx", "Villa.pptx");
         }
 
         public IActionResult Privacy()
